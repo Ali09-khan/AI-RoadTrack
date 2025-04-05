306401d7f81f392030099ec1e0bd46d8bababb22
Ali09-khan <84173641+Ali09-khan@users.noreply.github.com>
2025-04-05 13:28:32 +0500

    Update start_service.sh

--- service_kickoff/start_service.sh
+++ service_kickoff/start_service.sh
@@ -4,7 +4,7 @@ cd /home/alikhan/Desktop/projects/project/service_kickoff
 # Start TorchServe with container-relative paths
 torchserve --start \
   --ts-config config.properties \
-  --model-store /home/alikhan/Desktop/projects/sergek_project_1/service_holder \
+  --model-store /home/alikhan/Desktop/projects/project/service_holder \
   --models laneLpService.mar,carDetectService.mar \
   --disable-token-auth \
   --ncs
